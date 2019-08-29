b9e6751558b26bc7ed6622fafcc7eea9d7c97753
Max <zixel37@gmail.com>
2019-08-30 00:17:19 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    $('.solutions_slider').slick({
+
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left_blue.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right_blue.svg"></button>'
+
+    });
+});
